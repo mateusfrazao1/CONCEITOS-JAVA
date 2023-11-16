57a2d325547efd2ad775369d51cab8d7b72ec45a
mateus correa <mateusfrazao12345@gmail.com>
2023-11-16 19:56:23 -0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList; //importando biblioteca nativa do java para gerar arrays dinamicos
+
+
+public class Main {
+    public static void main(String[] args) {
+       arraylist <integer> idades = new arraylist<integer>();
+       idades.add(22); //adiciona valores ao array dinamico
+       idades.add(34); //adicionando outro valor ao array dinamico
+       idades,get(1);  //usamos o get para obter os valores dos arrays
+       idades,size();  //usamos size para obter o tamanho dos arrays que declaramos
+
+
+    }
+}
